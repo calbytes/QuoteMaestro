55ca0daccc17e7ee6b0072ecc3233b29c32543da
calbytes <caalla3@gmail.com>
2023-11-13 16:03:32 -0500

    flask POC for web server

--- populate_db_app.py
+++ populate_db_app.py
@@ -1,18 +1,11 @@
 import quote_factory.tokenizer as qt
 import db_manager.insert_quotes as db
-import db_manager.db_queries as db_queries
 
 def insert_quote_entries():
     quotes = qt.get_quote_entries()
     db.populate_db(quotes)
     print('insert_quote_entries() finished')
-
-def fetch_all():
-    db_queries.fetch_all()
-
-def count_entries():
-    db_queries.count_entries()
-
+    
 if __name__ == '__main__':
     #insert_quote_entries()
     print('qm_app() finished')

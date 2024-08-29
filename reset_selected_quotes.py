b0871e8a198f28e2618ba2119e5e51746fee2dc7
calbytes <caalla3@gmail.com>
2024-08-28 21:18:55 -0400

    refactored project

--- reset_selected_quotes.py
+++
@@ -1,7 +0,0 @@
-def reset_selected_quotes(): #TODO set all quotes.selected = false
-    print('calling db.fetch_all()')
-
-
-if __name__ == '__main__':
-    reset_selected_quotes()
-    
--- update_quote.py
+++
@@ -1,7 +0,0 @@
-
-def update_quote(): #TODO some quotes contain errors, updated the quote by quote.id
-    print('oh')
-
-if __name__ == '__main__':
-    update_quote()
-    
